67805691d7830616f3d30cb20c3f51d2f3ff417a
Omid <o.zare70@gmail.com>
2023-10-08 23:58:46 +0200

    separate logger module for better scalability

--- payment-logger-service/src/main/java/com/payment/logger/PaymentLoggerServiceApplication.java
+++ payment-logger-service/src/main/java/com/payment/logger/PaymentLoggerServiceApplication.java
@@ -1,6 +1,5 @@
 package com.payment.logger;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
