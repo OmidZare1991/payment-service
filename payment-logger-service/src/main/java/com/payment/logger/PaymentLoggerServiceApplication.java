4f2915058c27e65ac5fcbe581802c467b93e5716
Omid <o.zare70@gmail.com>
2023-10-08 23:58:09 +0200

    separate logger module for better scalability

---
+++ payment-logger-service/src/main/java/com/payment/logger/PaymentLoggerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.payment.logger;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PaymentLoggerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentLoggerServiceApplication.class, args);
+    }
+
+}
---
+++ payment-logger-service/src/test/java/com/payment/logger/PaymentLoggerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentLoggerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
